409073be66d9f4704eed3ef3ec03771bdc7c1109
case-dubs <casey594@gmail.com>
2014-03-13 16:38:24 -0700

    about to change index.html file to .php

---
+++ javascripts/application.js
@@ -0,0 +1,107 @@
+
+
+$(document).ready(function() {
+	
+	$('#landingFeatureSection').delay(500).fadeIn(1000);
+	console.log('in jquery file!');
+
+	/* hover-changing photo of me commented out: $('.imageHolder').on('mouseenter', function(){
+		$('#serious').hide();
+		$('#silly').show();
+	});
+
+	$('.imageHolder').on('mouseleave', function(){
+		$('#silly').hide();
+		$('#serious').fadeIn();
+	});*/
+	
+// function toBottom(){
+// window.scrollTo(150, document.body.scrollHeight);
+// }
+
+// 	$('#submit2').click(function(){
+// 		$('html, body').animate({
+// 			scrollTop: $(document).height(100px)}, "fast");
+// 			return false
+// 	});
+
+	var skillNormalize = function(){
+		$('.blurbs').hide();
+		$('.skillTitles').hide();
+		$('.skillImgs').hide();
+		$('.skillImgsShow').hide();
+		$('#introBlurb').fadeIn();
+	};
+
+	$('#skillsTop').on('click', function(){
+		skillNormalize();
+	});
+
+	$('#BinC').on('click', function(){
+		skillNormalize();
+		//$('#introBlurb').hide();
+		$('#chinaTitle').fadeIn();
+		$('#chinaBlurb').fadeIn();
+		$('#chinaImg').fadeIn();
+	});
+
+$('#branding').on('click', function(){
+		skillNormalize();
+		//$('#introBlurb').hide();
+		$('#brandingTitle').fadeIn();
+		$('#brandingBlurb').fadeIn();
+		$('#brandingImg').fadeIn();
+	});
+
+	
+
+	$('#BD').on('click', function(){
+		skillNormalize();
+	//	$('#introBlurb').hide();
+		$('#BDTitle').fadeIn();
+		$('#BDBlurb').fadeIn();
+		$('#BDImg').fadeIn();
+	});
+
+	$('#product').on('click', function(){
+		skillNormalize();
+		//$('#introBlurb').hide();
+		$('#productTitle').fadeIn();
+		$('#productBlurb').fadeIn();
+		$('#productImg').fadeIn();
+	});
+
+	/*$('#OrgDev').on('click', function(){
+		skillNormalize();
+		$('#introBlurb').hide();
+		$('#brandingTitle').fadeIn();
+		$('#brandingBlurb').fadeIn();
+		$('#brandingImg').fadeIn();
+	});
+	
+	$('#investment').on('click', function(){
+		skillNormalize();
+		$('#introBlurb').hide();
+		$('#brandingTitle').fadeIn();
+		$('#brandingBlurb').fadeIn();
+		$('#brandingImg').fadeIn();
+	});
+
+	$('#mgmt').on('click', function(){
+		skillNormalize();
+		$('#introBlurb').hide();
+		$('#brandingTitle').fadeIn();
+		$('#brandingBlurb').fadeIn();
+		$('#brandingImg').fadeIn();
+	});
+
+	$('#community').on('click', function(){
+		skillNormalize();
+		$('#introBlurb').hide();
+		$('#brandingTitle').fadeIn();
+		$('#brandingBlurb').fadeIn();
+		$('#brandingImg').fadeIn();
+	});*/
+
+
+});
